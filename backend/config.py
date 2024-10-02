05a7a93390e7d1dd15684823997dd2762bc864fe
John-thabuks <thabuksjohn@gmail.com>
2024-10-02 08:37:20 +0300

    fix(config): Setting up the config file

--- backend/config.py
+++ backend/config.py
@@ -3,27 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_cors import CORS
+from sqlalchemy import MetaData
 
 # Flask
 app = Flask(__name__)
 
-#Database
+metadata = MetaData()
+
+# Database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wave.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy()
 db.init_app(app)
 
-
-#Encrypt
+# Encrypt
 bcrypt = Bcrypt(app)
 
-#Migrate
-migrate = Migrate(app)
+# Migrate
+migrate = Migrate(app, db)
 
 
-#CORS
+# CORS
 cors = CORS(app)
 
-#Send request in Json
+# Send request in Json
 app.json.compact =False
 
