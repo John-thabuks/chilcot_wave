ac17b7bde05a888088dfd57638e2bb17241751a7
John-thabuks <thabuksjohn@gmail.com>
2024-10-03 14:03:14 +0300

    refactor(api): Added the Admin and Staff skeleton feature

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,28 @@
 from config import app, db
 from models import Users, Admin, Staff,CurrencyEnum, Customer, Vendor, Invoice, Purchase, VatEnum, Item, Category, SerialNumber, Currency, Lpo, PaymentModeEnum, Payment, Quotation, DeliveryNote, JobCardStatus, JobCard
+from flask_restful import Resource, Api
 
+api = Api(app)
+
+# Admin features
+class Admin(Resource):
+    pass
+
+
+
+# Staff features
+class Staff(Resource):
+    pass    
+
+
+
+
+
+
+
+
+api.add_resource(Admin, "/admin")
+api.add_resource(Staff, "/staff")
 
 
 
