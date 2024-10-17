f61470a2defd4754c950cdb3b141267df40e1b9e
John-thabuks <thabuksjohn@gmail.com>
2024-10-17 04:40:32 +0300

    scope(Login): Login route with JWT-Extended

--- backend/app.py
+++ backend/app.py
@@ -1,42 +1,38 @@
 from config import app, db
 from models import Users, Admin, Staff,CurrencyEnum, Customer, Vendor, Invoice, Purchase, VatEnum, Item, Category, SerialNumber, Currency, Lpo, PaymentModeEnum, Payment, Quotation, DeliveryNote, JobCardStatus, JobCard
 from flask_restful import Resource, Api
+from flask_jwt_extended import JWTManager, create_access_token
+from flask import request, jsonify
+from datetime import timedelta
 
 api = Api(app)
 
-# Admin features
-class Admin(Resource):
-    pass
+jwt = JWTManager(app)
 
 
 
-# Staff features
-class Staff(Resource):
-    pass    
+# Login 
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email")
+    password = request.json.get("password")
 
+    user = Users.query.filter_by(email=email).first()
 
-#Invoice
-class Invoice(Resource):
-    pass
-
-class Purchases(Resource):
-    pass
-
-class DeliveryNote(Resource):
-    pass
-
-api.add_resource(Admin, "/admin")
-api.add_resource(Staff, "/staff")
-api.add_resource(Invoice, "/invoice")
-api.add_resource(Purchase, "/purchase")
-api.add_resource(DeliveryNote, "/delivery_note")
+    if user and user.authenticate_password(password):
+        access_token = create_access_token(
+            identity={"email":email},
+            expires_delta=timedelta(hours=1),    #Token expires after 1 hour
+            fresh=True
+            )
+        
+        return jsonify(access_token=access_token)
+    else:
+        return jsonify({"message":"Bad credentials"}, 401)
 
 
 
 
-@app.route("/")
-def index():
-    return "Hello there"
 
 if __name__ == "__main__":
     app.run(debug=True)
